349979e0c1966a01b248dbc9752e2aee95b55874
arsx9 <sunnysheikh303@gmail.com>
2026-05-10 18:03:21 +0500

    tailwind added

--- job-portal-web/src/App.tsx
+++ job-portal-web/src/App.tsx
@@ -10,6 +10,9 @@ function App() {
   return (
     <>
       <section id="center">
+        <h1 className="text-3xl font-bold underline">
+          Hello From Tailwind!
+        </h1>
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
